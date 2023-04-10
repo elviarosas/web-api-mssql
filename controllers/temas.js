63fad4ee46cdfc045308bcabe7b02d98576bdbf3
Elvia Rosas Herrera <elviarosas@tec.mx>
2023-04-10 16:08:20 -0600

    se agregan rutas completas para CRUD

--- controllers/temas.js
+++ controllers/temas.js
@@ -16,7 +16,114 @@ module.exports = {
           }
         });
     } catch (err) {
-      return res.json({ message: `Erro al obtener los temas. Err: ${err}` });
+      return res
+        .status(500)
+        .json({ message: `Erro al obtener los temas. Err: ${err}` });
+    }
+  },
+  getTema: async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+      const pool = await poolPromise;
+      const result = await pool.request().query(
+        `SELECT * 
+                FROM Tema 
+                WHERE id = ${id}`,
+        function (err, resultset) {
+          if (err) {
+            console.log(err);
+          } else {
+            var temas = resultset.recordset;
+            return res.status(200).json(temas);
+          }
+        }
+      );
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: `Erro al obtener el tema. Err: ${err}` });
+    }
+  },
+  addTema: async (req, res, next) => {
+    const { title, description, imagen } = req.body;
+
+    console.log(title);
+    console.log(description);
+    console.log(imagen);
+
+    try {
+      const pool = await poolPromise;
+      const sql_query = `INSERT INTO Tema(title, description, imagen) 
+      VALUES ('${title}', '${description}', '${imagen}')`;
+      console.log(sql_query);
+      const result = await pool
+        .request()
+        .query(sql_query, function (err, resultset) {
+          if (err) {
+            console.log(err);
+            res
+              .status(500)
+              .json({ message: `Erro al agregar el Tema. Err: ${err}` });
+          } else {
+            var temas = resultset.recordset;
+            return res.status(200).json(temas);
+          }
+        });
+    } catch (err) {
+      return res.json({ message: `Erro al obtener el tema. Err: ${err}` });
+    }
+  },
+  updateTema: async (req, res, next) => {
+    const id = req.params.id;
+    const { title, description, imagen } = req.body;
+
+    try {
+      const pool = await poolPromise;
+      const result = await pool.request().query(
+        `UPDATE Tema
+        SET title = '${title}',
+        description = '${description}',
+        imagen = '${imagen}'
+        WHERE id = ${id}`,
+        function (err, resultset) {
+          if (err) {
+            console.log(err);
+            res
+              .status(500)
+              .json({ message: `Error al modificar el Tema. Err: ${err}` });
+          } else {
+            var temas = resultset.recordset;
+            return res.status(200).json(temas);
+          }
+        }
+      );
+    } catch (err) {
+      return res.json({ message: `Erro al obtener el tema. Err: ${err}` });
+    }
+  },
+  deleteTema: async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+      const pool = await poolPromise;
+      const result = await pool.request().query(
+        `DELETE Tema
+        WHERE id = ${id}`,
+        function (err, resultset) {
+          if (err) {
+            console.log(err);
+            res
+              .status(500)
+              .json({ message: `Error al borrar el Tema. Err: ${err}` });
+          } else {
+            var temas = resultset.recordset;
+            return res.status(200).json(temas);
+          }
+        }
+      );
+    } catch (err) {
+      return res.json({ message: `Erro al borrar el tema. Err: ${err}` });
     }
   },
 };
--- routes/temas.js
+++ routes/temas.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const UserControllers = require("../controllers/temas.js");
 
 router.get("/", UserControllers.getAllTemas);
-/*router.get('/:id', UserControllers.getUser)
-router.post('/add', UserControllers.addUser)
-router.put('/update/:id', UserControllers.updateUser)*/
+router.get("/:id", UserControllers.getTema);
+router.post("/add", UserControllers.addTema);
+router.put("/update/:id", UserControllers.updateTema);
+router.delete("/delete/:id", UserControllers.deleteTema);
 module.exports = router;
