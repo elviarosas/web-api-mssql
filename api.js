e37e2f3348f0afd51d0dcc4daa2d6bf791367629
Elvia Rosas Herrera <elviarosas@tec.mx>
2023-04-11 12:57:30 -0600

    actualizacion del cors para llmadas desde React

--- api.js
+++ api.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const dotenv =require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Pagina inicial WebApi')
-})
+app.get("/", (req, res) => {
+  res.send("Pagina inicial WebApi");
+});
 
 //Aqui estan todas las rutas
-app.use(require('./routes/routes.js'))
+app.use(require("./routes/routes.js"));
 
 app.listen(port, () => {
-  console.log(`Escuchando en el puerto ${port}`)
-})
+  console.log(`Escuchando en el puerto ${port}`);
+});
